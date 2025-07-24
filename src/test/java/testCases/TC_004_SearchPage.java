2c975e6565cbd960cf7545cf6f1659f338706a2b
Venkat <ramanaenjeti1989@gmail.com>
2025-07-24 15:35:18 +0530

    second commit

---
+++ src/test/java/testCases/TC_004_SearchPage.java
@@ -0,0 +1,13 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TC_004_SearchPage {
+	
+	// seach functionality
+	@Test
+	public void amethod() {
+		System.out.println("amehtod");
+	}
+
+}
